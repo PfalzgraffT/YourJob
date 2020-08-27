4c09b47a334379a6c2186a3c00b1410b7d6b04cf
Thomas Pfalzgraff <pfalzgraff.t@gmail.com>
2020-08-27 18:22:35 +0200

    Algorithme V1 OK

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,12 +1,15 @@
 class OffersController < ApplicationController
   def index
     # @offers = Offer.all
-    @offers = Offer.geocoded
+
+    @user = current_user
+    @offers = Offer.select('offers.*', "#{@user.matching_percentage_calc} AS matching_percentage").order('matching_percentage DESC').geocoded
     @markers = @offers.map do |offer|
       {
         lat: offer.latitude,
         lng: offer.longitude
       }
+
     end
   end
 
