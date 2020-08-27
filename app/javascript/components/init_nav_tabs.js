e790799670ab6d2c11dcfdd550b92a5af66b599c
Sophie Labouysse <sophie.labouysse@gmail.com>
2020-08-27 18:35:17 +0200

    comment js

--- app/javascript/components/init_nav_tabs.js
+++ app/javascript/components/init_nav_tabs.js
@@ -1,12 +1,12 @@
-const initNavTabs = () => {
-    const profileShow = document.getElementById('profile-card');
-    if (profileShow) {
-        $('#nav-tabs-wrapper a[data-toggle="tab"]').on('click', function(e) {
+// const initNavTabs = () => {
+//     const profileShow = document.getElementById('profile-card');
+//     if (profileShow) {
+//         $('#nav-tabs-wrapper a[data-toggle="tab"]').on('click', function(e) {
 
-            e.preventDefault();
+//             e.preventDefault();
             
-            $(e.target).closest('ul').hide().prev('a').removeClass('open').text($(this).text()); 
-        });
-    }
-  };
-  export { initNavTabs };
+//             $(e.target).closest('ul').hide().prev('a').removeClass('open').text($(this).text()); 
+//         });
+//     }
+//   };
+//   export { initNavTabs };
