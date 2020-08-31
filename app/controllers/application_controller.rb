b947783332fbf5aafe84bafbe40815b5690248f4
Sophie Labouysse <sophie.labouysse@gmail.com>
2020-08-31 18:46:16 +0200

    did something with the sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :first_name) }
+  # end
+  # def sign_up_params
+  #   devise_parameter_sanitizer.sanitize(:first_name)
+  # end
+
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:first_name)
+  # end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+  end
 end
