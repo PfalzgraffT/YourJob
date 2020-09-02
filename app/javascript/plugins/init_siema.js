4076f5f73f3ba93fc789ee0d6731c612491dd845
Sophie Labouysse <sophie.labouysse@gmail.com>
2020-09-02 12:24:28 +0200

    profile is lit

--- app/javascript/plugins/init_siema.js
+++ app/javascript/plugins/init_siema.js
@@ -1,15 +1,25 @@
 import Siema from 'siema';
 
 const initSiema = () => {
-    const siema = new Siema();
-    document.querySelector('.prev').addEventListener('click', (event) => {
-        event.preventDefault();
-        siema.prev()
-    });
-    document.querySelector('.next').addEventListener('click', (event) => {
-        event.preventDefault();
-        siema.next()
-    });
+    const slider = document.querySelector('.siema');
+    if (slider) {
+        let startIndex = 0;
+        const firstUncompletedSkillIndex = Array.from(document.querySelectorAll('.skill-card')).findIndex((card) => {
+            return card.dataset.skillCompleted === 'false'
+        });
+        if (firstUncompletedSkillIndex !== -1) { // si la function findIndex a trouve un element
+            startIndex = firstUncompletedSkillIndex;
+        }
+        const siema = new Siema({ startIndex });
+        document.querySelector('.prev').addEventListener('click', (event) => {
+            event.preventDefault();
+            siema.prev()
+        });
+        document.querySelector('.next').addEventListener('click', (event) => {
+            event.preventDefault();
+            siema.next()
+        });
+    }
 };
 
 export { initSiema };
